4560c29927add17117949941e60bce61bc04c1e8
Nghinguyenhuu <nghinguyenhuu@gmail.com>
2022-06-09 21:42:15 +0700

    done collections_enum

---
+++ collections_enum/bin/collections_enum.dart
@@ -0,0 +1,21 @@
+
+// ignore_for_file: unused_local_variable
+
+import 'dart:io';
+enum Weekday {
+  Monday, 
+  Tuesday, 
+  Wednesday, 
+  Thursday,
+  Friday, 
+  Saturday, 
+  Sunday
+}
+int main(List<String> args) {
+
+  for (var day in Weekday.values){
+    (day.index==5 || day.index == 6) ? print('${day.name} Holiday') : print('${day.name} not Holiday');
+  }
+  return 1;
+
+}
---
+++ collections_enum/test/collections_enum_test.dart
@@ -0,0 +1,8 @@
+import 'package:collections_enum/collections_enum.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
